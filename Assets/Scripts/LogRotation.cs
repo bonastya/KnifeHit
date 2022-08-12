0d9b21bb7c5ca24464be698c8ac3134b2c7a6f3c
bonastya <bonastya@gmail.com>
2022-08-12 21:49:14 +0400

    initial + log ang knife logic

---
+++ Assets/Scripts/LogRotation.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogRotation : MonoBehaviour
+{
+    [System.Serializable]
+    private class RotationElement
+    {
+        #pragma warning disable 0649
+        public float Speed;
+        public float Duration;
+        #pragma warning restore 0649
+    }
+
+    [SerializeField]
+    private RotationElement[] rotationPattern;
+
+    private WheelJoint2D WheelJoint;
+    private JointMotor2D motor;
+
+    private void Awake()
+    {
+        WheelJoint = GetComponent<WheelJoint2D>();
+        motor = new JointMotor2D();
+        StartCoroutine("PlayRotationPattern");
+
+    }
+
+    private IEnumerator PlayRotationPattern()
+    {
+        int rotationIndex = 0;
+
+        while (true)
+        {
+
+            yield return new WaitForFixedUpdate();
+
+            motor.motorSpeed = rotationPattern[rotationIndex].Speed;
+            motor.maxMotorTorque = 1000;
+            WheelJoint.motor = motor;
+
+            yield return new WaitForSecondsRealtime(rotationPattern[rotationIndex].Duration);
+            rotationIndex++;
+            rotationIndex = rotationIndex < rotationPattern.Length ? rotationIndex : 0;
+
+
+
+        }
+
+    }
+
+
+}
